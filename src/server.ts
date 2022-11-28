47af756c85e66e712551142a5218f0930edfadd3
yasith <it19966922@my.sliit.lk>
2022-11-28 10:21:11 +0530

    create prisma modals

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import express from 'express';
 const app = express();
 import router from './router'
 
-app.get("/", (req, res) => {
-    console.log("GET /");
-});
+// app.get("/", (req, res) => {
+//     console.log("GET /");
+// });
 
 app.use('/api', router)
 
