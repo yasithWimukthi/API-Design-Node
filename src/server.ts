5594f8584b4b18b08ac9dd57893b403143604ea2
yasith <it19966922@my.sliit.lk>
2022-11-29 09:38:37 +0530

    implement JWT

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import router from './router'
 
 
@@ -8,6 +9,7 @@ import router from './router'
 // });
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
